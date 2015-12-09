f60173a36c9de682f760a49c4a3e6e9ffdfc7e21
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-12-09 13:59:57 -0800

    refactor injector to fix #3

--- src/injection/index.js
+++ src/injection/index.js
@@ -4,6 +4,8 @@ var Promise = require('bluebird');
 var bodyParser = require('./body-parser');
 var ipc = require('electron').ipcRenderer;
 
+ipc.on('init', init);
+
 function init() {
   window.$ = require('jquery');
   window.gmail = require('./gmail')(window.$)
@@ -13,57 +15,51 @@ function init() {
     gmail.dom.messageContainers().each(function(i, el) {
       var container = $(el);
       // is this marked? if so we ignore it
-      if (container.data('electron') === 'ignore') return false;
-      // add decrypt button if it looks like pgp content
-      addButtonToContainer(container)
-      // mark it so we dont mess with it again
-      container.data('electron', 'ignore');
+      if (container.data('electron') !== 'marked') {
+        // we want to add decrypt button if it looks like pgp content
+        // this serves the dual purpose of filtering out message containers that are collapsed
+        // so that we don't accidentally mark them before actually placing a button
+        // if it is collapsed, we will get an empty jQuery array from extractMessageFromContainer
+
+        var bodyElement = gmail.dom.extractMessageFromContainer(container)
+        if (bodyElement.length === 1) {
+          var cryptoBlocks = getCryptoBlocks(bodyElement.text());
+          if (cryptoBlocks.length > 0) {
+            var decryptButton = addDecryptButtonToContainer(container, function() {
+              decrypt(cryptoBlocks).then(function(plaintexts) {
+                var plaintexts = plaintexts.join('\n---\n')
+                bodyElement.empty()
+                _.each(plaintexts, function(plaintext) {
+                  var html = plaintext.split('\n').join('<br>')
+                  bodyElement.append(html);
+                });
+                decryptButton.remove();
+              }).catch(function(err) {
+                showModal('Cannot decrypt!', 'Either you do not have the key, or your passphrase is wrong, or the message is corrupt.');
+              });
+            })
+          }
+          // mark it so we dont mess with it again
+          container.data('electron', 'marked');
+        }
+      }
     })
   }, 2000);
 }
 
-// message body 
-// $('.adP.adO')
-//
-// message container
-// $('.G2.G3')
-//
-//
 
-ipc.on('init', init);
-
-function addButtonToContainer(container) {
-  gmail.tools.addButtonToContainer(container, "Decrypt", function() {
-    var email = gmail.dom.extractMessageFromContainer(container)
-    var cryptoBlocks = getCryptoBlocks(email);
-    if (cryptoBlocks) {
-      decrypt(cryptoBlocks, email);
-    } else {
-      showModal('No Ciphertext', 'There is nothing to decrypt right now.');
-    }
-  });
+function addDecryptButtonToContainer(container, callback) {
+  return gmail.tools.addButtonToContainer(container, "Decrypt", callback);
 }
 
-function getCryptoBlocks(emailElement) {
-  var data = emailElement.text()
-  var pgpBlocks = bodyParser.extractPGP(data)
-  return pgpBlocks.length > 0 ? pgpBlocks : null
+function getCryptoBlocks(emailBody) {
+  return bodyParser.extractPGP(emailBody)
 }
 
-function decrypt(pgpBlocks, emailElement) {
+function decrypt(pgpBlocks) {
   return Promise.map(pgpBlocks, function(pgpMessage, idx) {
-    return pgp.decrypt(pgpMessage).catch(function(err) {
-      console.log(err);
-      showModal('Cannot decrypt!', 'Either you do not have the key, or your passphrase is wrong, or the message is corrupt.');
-    });
-  }).then(function(plaintexts) {
-    var plaintexts = plaintexts.join('\n---\n')
-    emailElement.empty()
-    _.each(plaintexts, function(plaintext) {
-      var html = plaintext.split('\n').join('<br>')
-      emailElement.append(html);
-    });
-  });
+    return pgp.decrypt(pgpMessage)
+  })
 }
 
 function showModal(title, body) {
