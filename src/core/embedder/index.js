2ce78966eb1cb440abe3434e3f46076a9fae540f
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-12-10 15:54:21 -0800

    basically done with tabs

--- src/core/embedder/index.js
+++ src/core/embedder/index.js
@@ -1,50 +1,62 @@
 "use strict";
-let mods = require('./src/mods');
-let config = require('./src/config');
+const mods = require('./src/mods');
+const config = require('./src/config');
+const preload = "src/core/injection/index.js";
+window.$ = require('jquery');
 
 let embeds = [];
 
 function init() {
   config.accounts.forEach(function(account) {
-    console.log(account);
     embeds.push({
       label: account.label,
       tab: createTab(account.label),
       webview: createWebview(account.label)
     });
   })
+  setTimeout(function() {
+    switchTo(config.accounts[0].label);
+  }, 0);
 }
 
 // TODO reveal/hide tab
 function switchTo(label) {
-  embeds.forEach(function(embed) {
-    if (label === embed.label) {
-      embed.webview.style['z-index'] = 9
-    } else {
-      embed.webview.style['z-index'] = 1
-    }
-  })
+  // highlight the tab
+  $('.tab').removeClass('active');
+  $(`.tab#tab-${label}`).addClass('active');
+  // surface the webview
+  $('.gmail').removeClass('active');
+  let webview = $(`.gmail#gmail-${label}`).addClass('active').get(0);
+  // set title to that of surfaced webview
+  $('title').text(webview.getTitle());
 }
 
 window.switchTo = switchTo
 
 // TODO create tab
 function createTab(label) {
-  console.log('creating tab', label);
+  let tab = $('<div>')
+  tab.attr('id', `tab-${label}`)
+  tab.addClass('tab')
+  tab.text(label);
+  $('.tabs').append(tab);
+  tab.click(function() {
+    switchTo(label);
+  });
+  return tab;
 }
 
 function createWebview(label) {
   let id = `gmail-${label}`;
   let src = "https://mail.google.com";
-  let preload = "src/core/injection/index.js";
-  let attrs = `id="${id}" src="${src}" preload="${preload}"`;
+  let attrs = `class="gmail" id="${id}" src="${src}" preload="${preload}"`;
   let html = `<webview ${attrs}></webview>`;
 
-  document.write(html);
+  $('.webviews').append(html);
   let webview = document.getElementById(id)
 
   webview.addEventListener('page-title-updated', function(e) {
-    document.getElementsByTagName("title")[0].text = e.title
+    $('title').text(e.title);
   });
 
   webview.addEventListener("dom-ready", function() {
