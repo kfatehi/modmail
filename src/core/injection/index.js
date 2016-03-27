a40cb1b2cdc7aa830569605712866bffa832981e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-03-27 15:27:40 -0700

    add stylesheet tool for use by injected mods

--- src/core/injection/index.js
+++ src/core/injection/index.js
@@ -9,6 +9,9 @@ ipc.on('init-injection', function(event, account) {
   let tools = {
     // react to dom events
     mutations: initMutationTool(),
+
+    // manipulate styles
+    stylesheet: initStyleTool(),
   }
 
   mods.initializeModComponents('injection', [account, tools]);
@@ -41,4 +44,20 @@ function initMutationTool() {
   }
 }
 
+function initStyleTool() {
+  // Create the <style> tag
+  var style = document.createElement("style");
+
+  // Add a media (and/or media query) here if you'd like!
+  // style.setAttribute("media", "screen")
+  // style.setAttribute("media", "only screen and (max-width : 1024px)")
+
+  // WebKit hack :(
+  style.appendChild(document.createTextNode(""));
+
+  // Add the <style> element to the page
+  document.head.appendChild(style);
+
+  return style.sheet;
+}
 
