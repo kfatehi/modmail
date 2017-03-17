1cdf0cc5a49997cbb7777dec5b92974ed340e946
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-03-17 08:59:42 -0700

    prevent false positive notifications

--- src/mods/notify/injection/index.js
+++ src/mods/notify/injection/index.js
@@ -2,40 +2,48 @@
 const fs = require('fs');
 var ipc = require('electron').ipcRenderer;
 
-module.exports.init = (api) => {
+module.exports.init = (api, account) => {
+  let count = 0;
   api.gmail.observe.on('load', ()=>{
     api.gmail.observe.on("refresh", ()=> {
-      setBadgeToUnreadInboxCount();
+      updateCount();
     });
 
-    api.gmail.observe.on("new_email", ()=> {
-      setBadgeToUnreadInboxCount();
-      notifyNewMail();
-    });
+    gmail.observe.on("new_email", function(id, url, body, xhr) {
+      // this is a little broken. none of the args work and it seems
+      // to repeatedly get called, so in order to prevent duplicates at least,
+      // we will only emit this if the count changed.
+      if (updateCount()) {
+        notifyNewMail();
+      }
+    })
 
     api.gmail.observe.on("read", ()=> {
-      setBadgeToUnreadInboxCount();
+      updateCount();
     });
 
-    setBadgeToUnreadInboxCount();
+    updateCount();
   });
 
-  function setBadgeToUnreadInboxCount() {
-    let count = gmail.get.unread_inbox_emails();
-    ipc.sendToHost('notify-set-unread-count', count);
+  function updateCount() {
+    let prev = count;
+    let newCount = gmail.get.unread_inbox_emails();
+    if ( prev === newCount ) return false;
+    else {
+      count = newCount;
+      ipc.sendToHost('notify-set-unread-count', count);
+      return true;
+    }
   }
 
   function notifyNewMail() {
-    let myNotification = new Notification('New Email', {
+    let myNotification = new Notification(account.label, {
       body: 'You have new mail.'
     })
 
     myNotification.onclick = () => {
-      ipc.sendToHost('new-mail-notification-clicked', count);
+      ipc.sendToHost('new-mail-notification-clicked');
     }
   }
-
-  window._setBadgeToUnreadInboxCount = setBadgeToUnreadInboxCount;
-  window._notifyNewMail = notifyNewMail;
 }
 
