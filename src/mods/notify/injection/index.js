faa504aa17381ff6a1df091cf7f924d97b361708
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-05-11 15:32:13 -0700

    mod: notify: fix notification bug and improve reliability

--- src/mods/notify/injection/index.js
+++ src/mods/notify/injection/index.js
@@ -5,34 +5,27 @@ var ipc = require('electron').ipcRenderer;
 module.exports.init = (api, account) => {
   let count = 0;
   api.gmail.observe.on('load', ()=>{
-    api.gmail.observe.on("refresh", ()=> {
-      updateCount();
-    });
-
-    gmail.observe.on("new_email", function(id, url, body, xhr) {
-      // this is a little broken. none of the args work and it seems
-      // to repeatedly get called, so in order to prevent duplicates at least,
-      // we will only emit this if the count changed.
-      if (updateCount()) {
-        notifyNewMail();
-      }
-    })
-
-    api.gmail.observe.on("read", ()=> {
-      updateCount();
-    });
-
+    api.gmail.observe.on("refresh", updateCount);
+    api.gmail.observe.on("read", updateCount);
+    api.gmail.observe.on("http_event", updateCount);
+    api.gmail.observe.on("new_email", updateCount);
+    api.gmail.observe.on("refresh", updateCount);
+    api.gmail.observe.on("poll", updateCount);
     updateCount();
   });
 
   function updateCount() {
     let prev = count;
     let newCount = gmail.get.unread_inbox_emails();
-    if ( prev === newCount ) return false;
-    else {
+    if ( prev !== newCount ) {
       count = newCount;
       ipc.sendToHost('notify-set-unread-count', count);
+      if ( prev < newCount ) {
+        notifyNewMail();
+      }
       return true;
+    } else {
+      return false;
     }
   }
 
